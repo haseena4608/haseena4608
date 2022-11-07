048a3789999838683d251f3e8ec734ef4fc73a75
haseena4608 <116862558+haseena4608@users.noreply.github.com>
2022-11-07 19:27:07 +0530

    Create py basic.py

---
+++ py basic.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(n):
+    if i%2!=0:
+        print("weird")
+    elif i%2==0 and range(2,5):
+        print("not weird")
+    elif i%2==0 and range(6,20):
+        print("weird")
+    elif i%2==0 and i>20:
+        print("not weird")
+    else:
+        print(" ")
+
+        
